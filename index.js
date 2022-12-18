862b8ad951afda8b293c14b1a5fecd6c74be8d4f
UtayaAzusa <utaya1023@gmail.com>
2022-12-18 10:28:58 +0900

    不要要素の削除

--- index.js
+++ index.js
@@ -3,37 +3,6 @@
 //   $("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
 // });
 
-let images = document.querySelectorAll(`.top_image`);
-
-for (const [key, value] of Object.entries(images)) {
-
-  value.animate(
-    [
-      {
-        offset: 0.0,
-        opacity: '0'
-      },
-      {
-        offset: 0.1,
-        opacity: '1'
-      },
-      {
-        offset: 0.9,
-        opacity: '1'
-      },
-      {
-        offset: 1.0,
-        opacity: '0'
-      }
-    ],
-    {
-      duration: 15000,
-      delay: key * 5000,
-      iterations: Infinity
-    }
-  );
-
-}
 
 // カウントダウン
 let countdown = setInterval(function () {
